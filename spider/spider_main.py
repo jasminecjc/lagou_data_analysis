df009640336c22bff3c6e3566b4f4f6c6c01e89c
jasminecjc <jasminecjc@ijugo.net>
2017-05-02 15:47:15 +0800

    update

--- spider/spider_main.py
+++ spider/spider_main.py
@@ -15,6 +15,23 @@ import sys
 reload(sys) 
 sys.setdefaultencoding('utf-8') 
 
+# 代理服务器
+proxyHost = "proxy.abuyun.com"
+proxyPort = "9010"
+
+# 代理隧道验证信息
+proxyUser = "H77D3H3989FF44PP"
+proxyPass = "4528FDF31F1B06BB"
+
+proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
+  "host" : proxyHost,
+  "port" : proxyPort,
+  "user" : proxyUser,
+  "pass" : proxyPass,
+}
+
+
+
 db = MySQLdb.connect(host="localhost", user="root", passwd="55556666", db="graduate_work", charset="utf8")
 cursor = db.cursor()
 
@@ -26,21 +43,23 @@ headers = {
     'Cookie': 'LGUID=20160223140504-620c0982-d9f3-11e5-8b4c-525400f775ce; tencentSig=6558885888; user_trace_token=20170228174718-e5fb608afce74f799b7776b1047078c6; fromsite=www.google.co.jp; index_location_city=%E5%85%A8%E5%9B%BD; SEARCH_ID=903d052305ac48cd89da8777f732ed0d; JSESSIONID=937760D5DB1DE2F81DA0B920D449CE9F; PRE_UTM=; PRE_HOST=www.baidu.com; PRE_SITE=https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3DgqHLbZOanuNKiqmYhHa76U6q7FKEDxhDErLFpCoECiO%26wd%3D%26eqid%3D8df355dc0004eb9b0000000258e7a7eb; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2F; TG-TRACK-CODE=index_company; _ga=GA1.2.507913091.1456207502; LGSID=20170407225346-009efbed-1ba2-11e7-9d24-5254005c3644; LGRID=20170407225432-1bd38d70-1ba2-11e7-9d24-5254005c3644; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1491466928,1491556280,1491576814,1491576825; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1491576870'
 }
     
-def get_proxy():
-    return requests.get("http://127.0.0.1:5000/get/").content
+# def get_proxy():
+#     return requests.get("http://127.0.0.1:5000/get/").content
 
-def delete_proxy(proxy):
-    requests.get("http://127.0.0.1:5000/delete/?proxy={}".format(proxy))
+# def delete_proxy(proxy):
+#     requests.get("http://127.0.0.1:5000/delete/?proxy={}".format(proxy))
 
 def valid_proxy(path, method, code = 0, *payload):
     while code != 200:
         try:  
-            proxy = get_proxy()
-            proxies = {"https": "https://{}".format(proxy)}
+            proxies = {
+                "http"  : proxyMeta,
+                "https" : proxyMeta,
+            }
             if method == 'get':     
-                source = session.get(path, headers = headers, proxies = proxies, timeout = 20)
+                source = session.get(path, headers = headers, proxies = proxies, timeout = 5)
             else:
-                source = session.post(path, headers = headers, proxies = proxies, data = payload[0], timeout = 20) 
+                source = session.post(path, headers = headers, proxies = proxies, data = payload[0], timeout = 5) 
             try:
                 notfound = len(source.json()['result'])
             except:
@@ -52,90 +71,156 @@ def valid_proxy(path, method, code = 0, *payload):
             print e
     return [source, proxies]
 # lagou
-    
+def company_clawer(i, path, position_path, payload, position_payload, company_res):
+    payload['pn'] = str(i)
+    company_source = partial(valid_proxy, path, 'post', 0)(payload)[0].json()
+    for company in company_source['result']:
+        company_id = company['companyId']
+        company_name = company['companyShortName']
+        company_city = company['city']
+        company_logo = company['companyLogo']
+        company_stage = company['financeStage']
+        company_pos = company['positionNum']
+        company_path = 'https://www.lagou.com/gongsi/%s.html' % (company_id)
+        company_home = partial(valid_proxy, company_path, 'get', 0)()[0]
+        soup = BeautifulSoup(company_home.content)
+        company_people = soup.select('.number span')[0].get_text()
+        company_intro = soup.select('.company_content span')[0].get_text()
+        tags = soup.select('.con_ul_li')
+        company_tags = []
+        for tag in tags:
+            company_tags.push(tag.get_text())
+        position_payload['companyId'] = company_id
+        salary = 0
+        for page in range(int(math.ceil(float(company_pos) / 10))):
+            position_payload['pageNo'] = str(page)
+            positions = partial(valid_proxy, position_path, 'post', 0)(payload)[0].json()['content']['data']['page']['result']
+            for position in positions:
+                if position['jobNature'] != '全职':
+                    continue
+                salary += aver_salary(position['salary'])
+        company_salary = salary / company_pos
+        company_res.push((company_name, company_city, company_logo, company_stage, company_pos, company_people, company_intro, company_tags, company_salary))  
+        try:  
+            cursor.executemany(company_sql, company_res) 
+            db.commit() 
+        except Exception, e:
+            db.rollback()
+            print 'except: sql'
+            print e 
 # 公司分析
 def companys():    
-    path = 'https://www.lagou.com/gongsi'
-    source = partial(valid_proxy, path, 'get', 0)()[0]
-    soup = BeautifulSoup(source.content)
-    #print soup   
-    company_total = soup.select('div.details')[0]
-    count = 0
-    company_value = []
+    path = 'https://www.lagou.com/gongsi/0-0-0.json'
+    position_path = 'https://www.lagou.com/gongsi/searchPosition.json'
     payload = {'first': 'false', 'pn': '2', 'sortField': '0', 'havemark': '0'}
-    companys_sql = '''insert into lagou_companys(level,
-                      tag, totalCount)
-                      values (%s, %s, %s)'''
-    #公司分析
-    for index, child in enumerate(company_total.children):
-        if isinstance(child, NavigableString):
-            count += 1
-            continue
-        child_class = child['class'][-1]
-        child_tags = soup.select('.%s a' % (child_class)) if child_class == 'financeStage' else company_total.select('.%s .hot a' % (child_class))
-        for i in child_tags[1::]:
-            data_id = i['data-id']
-            init_index = ['0', '0', '0']
-            init_index[index - count] = str(data_id)
-            index_param = '-'.join(init_index)
-            industry_path = '%s/%s.json' % (path, index_param)
-            companys = partial(valid_proxy, industry_path, 'post', 0)(payload)[0].json()
-            company_value.append((i.get_text().strip(), child_class, int(companys['totalCount'])))
+    position_payload = {'positionFirstType': '全部', 'pageSize': '10'}
+    source = partial(valid_proxy, path, 'post', 0)(payload)[0].json()
+    company_pages = int(math.ceil(int(source['totalCount']) / int(source['pageSize'])))
+    company_res = []
+    company_sql = '''insert into lagou_company(name, 
+                     city, 
+                     logo_address, 
+                     finance_stage, 
+                     position_num, 
+                     people_num, 
+                     introduce, 
+                     tags, 
+                     aver_salary)
+                     values (%s ,%s ,%s ,%s ,%s, %s, %s, %s, %s)'''
     
-    try:                     
-        cursor.executemany(companys_sql, company_value)    
-        db.commit() 
+    
+    try:
+        for i in range(2, company_pages, company_pages / 5):   
+            t = threading.Thread(target=company_crawler,
+                              args=(i, path, position_path, payload, position_payload, company_res))
+            thread.append(t)
+        for i in range(0,6):
+            thread[i].start()
+        for i in range(0,6):
+            thread[i].join()
     except Exception, e:
-        db.rollback()
-        print 'except: sql'
-        print e     
-    #C轮以及以上公司行业
-    stage_index = '0-5,6,7-0'
-    stage_path = '%s/%s.json' % (path, stage_index)
-    res = partial(valid_proxy, stage_path, 'post', 0)(payload)
-    stage_companys = res[0].json()
-    proxies = res[1]
-    stage_count = int(stage_companys['totalCount'])
-    stage_pages = int(math.ceil(stage_count / int(stage_companys['pageSize'])))
-    fields_dic = dict()
-    fw = open('company_feature.txt', 'w+')
-    for i in range(2, stage_pages):
-        #临界值60
-        payload['pn'] = str(i)
-        if i % 50 == 0:
-            proxies = {"https": "https://{}".format(get_proxy())}
-        code = 0
-        while code != 200:
-            try:  
-                pn_companys = session.post(stage_path, headers = headers, proxies = proxies, data = payload, timeout = 20).json()
-                code = 200 if len(pn_companys['result']) != 0 else 0
-                print i
-            except Exception, e:
-                print 'except: 2'
-                proxies = {"https": "https://{}".format(get_proxy())}
-        print pn_companys['pageNo']
-        for details in pn_companys['result']:
-            feature = details['companyFeatures']
-            fw.write(feature)
-            fields = details['industryField'].strip()
-            if re.search(ur"[%s]+" %punctuation, fields):
-                fields = re.sub(ur"[%s]+" %punctuation, ",", fields)
-            fields = fields.split(',')
-            for field in fields:
-                if field.strip() not in fields_dic:
-                    fields_dic[field.strip()] = 1
-                fields_dic[field.strip()] += 1
-    fw.close()
-    fields_value = []
-    for (key, val) in fields_dic.items():
-        fields_value.append((key, 'field', val))
-    try:  
-        cursor.executemany(companys_sql, fields_value) 
-        db.commit() 
-    except Exception, e:
-        db.rollback()
-        print 'except: sql'
-        print e
+        print 'except: 7'
+        print e  
+    
+    #soup = BeautifulSoup(source.content)
+    #print soup   
+    # company_total = soup.select('div.details')[0]
+    # count = 0
+    # company_value = []
+    # payload = {'first': 'false', 'pn': '2', 'sortField': '0', 'havemark': '0'}
+    # companys_sql = '''insert into lagou_companys(level,
+    #                   tag, totalCount)
+    #                   values (%s, %s, %s)'''
+    # #公司分析
+    # for index, child in enumerate(company_total.children):
+    #     if isinstance(child, NavigableString):
+    #         count += 1
+    #         continue
+    #     child_class = child['class'][-1]
+    #     child_tags = soup.select('.%s a' % (child_class)) if child_class == 'financeStage' else company_total.select('.%s .hot a' % (child_class))
+    #     for i in child_tags[1::]:
+    #         data_id = i['data-id']
+    #         init_index = ['0', '0', '0']
+    #         init_index[index - count] = str(data_id)
+    #         index_param = '-'.join(init_index)
+    #         industry_path = '%s/%s.json' % (path, index_param)
+    #         companys = partial(valid_proxy, industry_path, 'post', 0)(payload)[0].json()
+    #         company_value.append((i.get_text().strip(), child_class, int(companys['totalCount'])))
+    
+    # try:                     
+    #     cursor.executemany(companys_sql, company_value)    
+    #     db.commit() 
+    # except Exception, e:
+    #     db.rollback()
+    #     print 'except: sql'
+    #     print e     
+    # #C轮以及以上公司行业
+    # stage_index = '0-5,6,7-0'
+    # stage_path = '%s/%s.json' % (path, stage_index)
+    # res = partial(valid_proxy, stage_path, 'post', 0)(payload)
+    # stage_companys = res[0].json()
+    # proxies = res[1]
+    # stage_count = int(stage_companys['totalCount'])
+    # stage_pages = int(math.ceil(stage_count / int(stage_companys['pageSize'])))
+    # fields_dic = dict()
+    # fw = open('company_feature.txt', 'w+')
+    # for i in range(2, stage_pages):
+    #     #临界值60
+    #     payload['pn'] = str(i)
+    #     if i % 50 == 0:
+    #         proxies = {"https": "https://{}".format(get_proxy())}
+    #     code = 0
+    #     while code != 200:
+    #         try:  
+    #             pn_companys = session.post(stage_path, headers = headers, proxies = proxies, data = payload, timeout = 20).json()
+    #             code = 200 if len(pn_companys['result']) != 0 else 0
+    #             print i
+    #         except Exception, e:
+    #             print 'except: 2'
+    #             proxies = {"https": "https://{}".format(get_proxy())}
+    #     print pn_companys['pageNo']
+    #     for details in pn_companys['result']:
+    #         feature = details['companyFeatures']
+    #         fw.write(feature)
+    #         fields = details['industryField'].strip()
+    #         if re.search(ur"[%s]+" %punctuation, fields):
+    #             fields = re.sub(ur"[%s]+" %punctuation, ",", fields)
+    #         fields = fields.split(',')
+    #         for field in fields:
+    #             if field.strip() not in fields_dic:
+    #                 fields_dic[field.strip()] = 1
+    #             fields_dic[field.strip()] += 1
+    # fw.close()
+    # fields_value = []
+    # for (key, val) in fields_dic.items():
+    #     fields_value.append((key, 'field', val))
+    # try:  
+    #     cursor.executemany(companys_sql, fields_value) 
+    #     db.commit() 
+    # except Exception, e:
+    #     db.rollback()
+    #     print 'except: sql'
+    #     print e
             
 # 职位分析
 def job_title():
@@ -215,7 +300,7 @@ def job_count(job_list, payload, path, job_sql, dic):
             db.rollback()
             print 'except: sql'
             print e        
-def data_job_count:
+#def data_job_count:
 
 def job_desc():
     job_dic = {
