1a4ea7778bd8af38a470755b70b72775ee68f300
Jiachen Cheng <jiachencheng@Jiachens-MacBook-Pro.local>
2017-05-14 21:35:57 +0800

    update

--- spider/spider_main.py
+++ spider/spider_main.py
@@ -57,6 +57,7 @@ def valid_proxy(path, method, code = 0, *payload):
                 "http"  : proxyMeta,
                 "https" : proxyMeta,
             }
+
             if method == 'get':     
                 source = session.get(path, headers = headers, proxies = proxies, timeout = 5)
             else:
@@ -149,21 +150,71 @@ def companys():
                      aver_salary)
                      values (%s ,%s ,%s ,%s ,%s, %s, %s, %s, %s, %s)'''
         
-    try:
-        thread = []
-        threadNum = 10 if company_pages % 5 == 0 else 6
-        ranges = company_pages / 10
-        for i in range(0, company_pages, company_pages / 5):   
-            t = threading.Thread(target=company_crawler,
-                              args=(i, ranges, path, position_path, payload, position_payload, company_sql))
-            thread.append(t)
-        for i in range(0, threadNum):
-            thread[i].start()
-        for i in range(0, threadNum):
-            thread[i].join()
-    except Exception, e:
-        print 'except: 7'
-        print e  
+    # try:
+    #     thread = []
+    #     threadNum = 10 if company_pages % 5 == 0 else 6
+    #     ranges = company_pages / 10
+    #     for i in range(0, company_pages, company_pages / 5):   
+    #         t = threading.Thread(target=company_crawler,
+    #                           args=(i, ranges, path, position_path, payload, position_payload, company_sql))
+    #         thread.append(t)
+    #     for i in range(0, threadNum):
+    #         thread[i].start()
+    #     for i in range(0, threadNum):
+    #         thread[i].join()
+    # except Exception, e:
+    #     print 'except: 7'
+    #     print e  
+    
+    for i in range(1, company_pages):
+        payload['pn'] = str(i)
+        company_source = partial(valid_proxy, path, 'post', 0)(payload)[0].json()       
+        print j
+        for company in company_source['result']:
+            try: 
+                company_res = []
+                company_id = company['companyId']
+                company_name = company['companyShortName']
+                company_city = company['city']
+                company_logo = company['companyLogo']
+                company_stage = company['financeStage']
+                company_pos = company['positionNum']
+                company_industry = company['industryField']
+                company_path = 'https://www.lagou.com/gongsi/%s.html' % (company_id)
+                company_home = partial(valid_proxy, company_path, 'get', 0)()[0]
+                time.sleep(0.1)
+                soup = BeautifulSoup(company_home.content, "html5lib")
+                company_people = soup.select('.number')[0].parent.get_text()
+                company_intro = soup.select('.company_content')[0].get_text()
+                tags = soup.select('.con_ul_li')
+                company_tags = []
+                for tag in tags:
+                    company_tags.append(tag.get_text())
+                position_payload['companyId'] = company_id
+                salary = 0
+                for page in range(int(math.ceil(float(company_pos) / 10))):
+                    position_payload['pageNo'] = str(page)
+                    positions = partial(valid_proxy, position_path, 'post', 0)(position_payload)[0].json()['content']['data']['page']['result']
+                    time.sleep(0.1)
+                    for position in positions:
+                        if position['jobNature'] != '全职':
+                            continue
+                        salary += aver_salary(position['salary'])
+                company_salary = salary / company_pos
+                company_res.append((company_name, company_city, company_logo, company_industry, company_stage, company_pos, company_people, company_intro, company_tags, company_salary))            
+                try:  
+                    cursor.execute(company_sql, company_res) 
+                    print 'sql'
+                    db.commit() 
+                except Exception, e:
+                    db.rollback()
+                    print 'except: sql'
+                    print e 
+            except Exception, e:
+                print 'except get company data'
+                print e
+    #print len(company_res)
+    
     
     #soup = BeautifulSoup(source.content)
     #print soup   
