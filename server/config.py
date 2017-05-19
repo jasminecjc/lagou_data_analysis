b28c47ffc5f296bea55375d0f4338f2b54b8bf31
Jiachen Cheng <jiachencheng@Jiachens-MacBook-Pro.local>
2017-05-19 09:41:22 +0800

    update

---
+++ server/config.py
@@ -0,0 +1,6 @@
+HOST = '59.110.227.27'
+PORT = 3306
+USER = 'root'
+PASSWORD = 'wjbxlcjc'
+DATABASE = 'graduate_work'
+CHARSET = 'utf8'
---
+++ server/run.py
@@ -0,0 +1,51 @@
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import config
+
+databaseurl =  'mysql://%s:%s@%s:%s/%s' % (config.USER, config.PASSWORD, config.HOST, config.PORT, config.DATABASE)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = databaseurl
+
+db = SQLAlchemy(app)
+
+class mytable(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    lan = db.Column(db.String(20), nullable=False)
+    city = db.Column(db.String(20), nullable=False)
+    aver_salary = db.Column(db.Integer, nullable=False)
+    years = db.Column(db.String(20), nullable=False)
+    finance_stage = db.Column(db.String(20), nullable=False)
+    education = db.Column(db.String(20), nullable=False)
+    fields = db.Column(db.String(20), nullable=False)
+
+    def __init__(self, id, lan, city):
+        self.id = id
+        self.lan = lan
+        self.city = city
+        self.aver_salary = aver_salary
+        self.years = years
+        self.finance_stage = finance_stage
+        self.education = education
+        self.fields = fields
+
+    def __repr__(self):
+        return '<Id %r User %r>' % (self.id, self.name)
+
+@app.route('/', methods=['GET'])
+def get_one():
+    if not request.args['id']:
+        abort(400)
+    get_id = request.args['id']
+    #得到表中所有的数据
+    ids = mytable.query.all()
+    #使用filter找到指定项目
+    get = mytable.query.filter_by(id = get_id).first()
+    #获取表成员属性
+    ret = 'id=%d,name=%s,age=%d' % (get.id, get.name, get.age)
+    return ret
+    
+app.run(debug = True)
