f9f09a124e7798fff0bfc3e655f3f37d521fcf48
jasminecjc <249485410@qq.com>
2017-05-30 11:02:51 +0800

    delete

--- config.py
+++
@@ -1,6 +0,0 @@
-HOST = 'localhost'
-PORT = 3306
-USER = 'root'
-PASSWORD = 'root'
-DATABASE = 'graduate_work'
-CHARSET = 'utf8'
--- server/config.py
+++
@@ -1,6 +0,0 @@
-HOST = '59.110.227.27'
-PORT = 3306
-USER = 'root'
-PASSWORD = 'wjbxlcjc'
-DATABASE = 'graduate_work'
-CHARSET = 'utf8'
